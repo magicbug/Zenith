7133967d64580a276c8f5762792f644bda11db33
Peter Goodhall <peter@magicbug.co.uk>
2025-04-10 14:52:18 +0100

    wip

--- api/amsat_status_proxy.php
+++ api/amsat_status_proxy.php
@@ -4,11 +4,20 @@
 // Get POST data
 $postData = json_decode(file_get_contents('php://input'), true);
 
+// Map frontend field names to expected field names
+$mappedData = [
+    'date' => $postData['timeOn'],
+    'satName' => $postData['satelliteName'],
+    'callsign' => $postData['stationCallsign'],
+    'status' => $postData['status'],
+    'gridSquare' => $postData['myGridsquare']
+];
+
 // Validate required fields
 $requiredFields = ['date', 'satName', 'callsign', 'status', 'gridSquare'];
 $missingFields = [];
 foreach ($requiredFields as $field) {
-    if (!isset($postData[$field]) || empty($postData[$field])) {
+    if (!isset($mappedData[$field]) || empty($mappedData[$field])) {
         $missingFields[] = $field;
     }
 }
@@ -24,26 +33,26 @@ if (!empty($missingFields)) {
 }
 
 // Extract time components directly from the frontend time string
-$timeParts = explode(' ', $postData['date']);
+$timeParts = explode(' ', $mappedData['date']);
 $dateParts = explode('-', $timeParts[0]);
 $timeComponents = explode(':', $timeParts[1]);
 
 // Debug the incoming time
-error_log("Incoming time from frontend: " . $postData['date']);
+error_log("Incoming time from frontend: " . $mappedData['date']);
 
 // Build the AMSAT status URL
 $url = 'https://amsat.org/status/submit.php?' . http_build_query([
     'SatSubmit' => 'yes',
     'Confirm' => 'yes',
-    'SatName' => $postData['satName'],
+    'SatName' => $mappedData['satName'],
     'SatYear' => $dateParts[0],
     'SatMonth' => str_pad($dateParts[1], 2, '0', STR_PAD_LEFT),
     'SatDay' => str_pad($dateParts[2], 2, '0', STR_PAD_LEFT),
     'SatHour' => str_pad($timeComponents[0], 2, '0', STR_PAD_LEFT),
     'SatPeriod' => (intdiv((int)$timeComponents[1] - 1, 15)),
-    'SatCall' => $postData['callsign'],
-    'SatReport' => $postData['status'],
-    'SatGridSquare' => $postData['gridSquare']
+    'SatCall' => $mappedData['callsign'],
+    'SatReport' => $mappedData['status'],
+    'SatGridSquare' => $mappedData['gridSquare']
 ]);
 
 error_log("Final URL with time components: " . $url);
