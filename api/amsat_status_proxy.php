b91bdaccee0a4e71920380acd432aa26c59d78ac
Peter Goodhall <peter@magicbug.co.uk>
2025-04-10 14:24:42 +0100

    wip

--- api/amsat_status_proxy.php
+++ api/amsat_status_proxy.php
@@ -4,21 +4,30 @@
 // Get POST data
 $postData = json_decode(file_get_contents('php://input'), true);
 
+// Extract time components directly from the frontend time string
+$timeParts = explode(' ', $postData['timeOn']);
+$dateParts = explode('-', $timeParts[0]);
+$timeComponents = explode(':', $timeParts[1]);
+
+// Debug the incoming time
+error_log("Incoming time from frontend: " . $postData['timeOn']);
+
 // Build the AMSAT status URL
-$dateTime = new DateTime($postData['timeOn'], new DateTimeZone('UTC'));
 $url = 'https://amsat.org/status/submit.php?' . http_build_query([
     'SatSubmit' => 'yes',
     'Confirm' => 'yes',
     'SatName' => $postData['satName'],
-    'SatYear' => $dateTime->format('Y'),
-    'SatMonth' => str_pad($dateTime->format('m'), 2, '0', STR_PAD_LEFT),
-    'SatDay' => str_pad($dateTime->format('d'), 2, '0', STR_PAD_LEFT),
-    'SatHour' => str_pad($dateTime->format('H'), 2, '0', STR_PAD_LEFT),
-    'SatPeriod' => (intdiv((int)$dateTime->format('i') - 1, 15)),
+    'SatYear' => $dateParts[0],
+    'SatMonth' => str_pad($dateParts[1], 2, '0', STR_PAD_LEFT),
+    'SatDay' => str_pad($dateParts[2], 2, '0', STR_PAD_LEFT),
+    'SatHour' => str_pad($timeComponents[0], 2, '0', STR_PAD_LEFT),
+    'SatPeriod' => (intdiv((int)$timeComponents[1] - 1, 15)),
     'SatCall' => $postData['callsign'],
     'SatReport' => $postData['status'],
     'SatGridSquare' => $postData['gridSquare']
 ]);
+
+error_log("Final URL with time components: " . $url);
 
 // Initialize cURL session
 $ch = curl_init($url);
