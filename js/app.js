5baa89a1327e356a17459cbc9c1f58d007fb0c18
Peter Goodhall <peter@magicbug.co.uk>
2025-04-22 14:10:16 +0100

    update upcoming less often

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@ window.tleData = {};
 // Constants
 const TLE_URL = 'api/fetch_tle.php';  // Changed to use our PHP proxy script
 const UPDATE_INTERVAL_MS = 5000; // Update satellite positions every 5 seconds
-const PASS_UPDATE_INTERVAL_MS = 60000; // Update passes every minute
+const PASS_UPDATE_INTERVAL_MS = 300000; // Update passes every minute
 const PASS_PREDICTION_HOURS = 24; // Predict passes for the next 24 hours
 const FOOTPRINT_POINTS = 36; // Number of points to draw the footprint circle
 
